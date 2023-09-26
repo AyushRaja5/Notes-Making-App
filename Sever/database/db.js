3deee2a40de34c9b47208074edb2a420fa568e32
AYUSH RAJA <101151963+AyushRaja5@users.noreply.github.com>
2023-09-26 22:42:52 +0530

    Update db.js

--- Sever/database/db.js
+++ Sever/database/db.js
@@ -1,11 +1,11 @@
 import { connect } from "mongoose";
 
-const url =  "mongodb+srv://AyushRaja:12345@merndatabase.cap6mjs.mongodb.net/eNotebook"
+const url =  "mongodb+srv://AyushRaja:*****@merndatabase.cap6mjs.mongodb.net/eNotebook"
 
 const connectToMongo = async() =>{
     try{
         await connect(url)
-        console.log("Db Connected Successfully")
+        console.log("Db Connected Successfully p(1-5)")
     }
     catch(error){
         console.log(error);
